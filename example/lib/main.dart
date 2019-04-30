aace2de014898bcefd7766a9312d23a7a5511eb5
Bhavik Makwana <bhavik.m@simformsolutions.com>
2019-04-30 11:17:14 +0530

    added gif preview, hide debug banner

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Credit Card View Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),

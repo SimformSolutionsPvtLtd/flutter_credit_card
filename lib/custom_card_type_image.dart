be494e3c1fc3b54be8d97cf98f21e55d1ecbd91a
Meet Janani <meet.j@simformsolutions.com>
2021-09-28 15:01:06 +0530

    :bug: #57 Can specify a custom card type icons

---
+++ lib/custom_card_type_image.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+
+class CustomCardTypeImage {
+  CustomCardTypeImage({
+    required this.cardType,
+    required this.cardImage,
+  });
+
+  CardType cardType;
+  Widget cardImage;
+}

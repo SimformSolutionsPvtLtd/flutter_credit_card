89cec694938a558f0469904e2052955ab3b949d9
DhavalRKansara <dhavalkansara51@gmail.com>
2022-07-25 16:29:07 +0530

    Fixed warnings in the code.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_brand.dart';
-import 'package:flutter_credit_card/credit_card_form.dart';
-import 'package:flutter_credit_card/credit_card_model.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 void main() => runApp(MySample());

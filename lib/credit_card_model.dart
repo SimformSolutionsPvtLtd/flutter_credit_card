6d840fb048fc2fb7253f6400606c5e22d9dc6963
ujas-m-simformsolutions <ujas.m@simformsolutions.com>
2023-03-01 15:43:41 +0530

    :memo: Adds documentation for public apis and :bug: fixes depreciated parameter usage

--- lib/credit_card_model.dart
+++ lib/credit_card_model.dart
@@ -2,9 +2,18 @@ class CreditCardModel {
   CreditCardModel(this.cardNumber, this.expiryDate, this.cardHolderName,
       this.cvvCode, this.isCvvFocused);
 
+  /// Number of the credit/debit card.
   String cardNumber = '';
+
+  /// Expiry date of the card.
   String expiryDate = '';
+
+  /// Name of the card holder.
   String cardHolderName = '';
+
+  /// Cvv code on card.
   String cvvCode = '';
+
+  /// A boolean for indicating if cvv is focused or not.
   bool isCvvFocused = false;
 }
